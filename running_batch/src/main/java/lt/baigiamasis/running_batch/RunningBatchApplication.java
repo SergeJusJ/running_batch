57d8a30c69328c520b86caa6e9e0a965c30ca400
SergeJusJ <sergeusj@gmail.com>
2022-08-18 18:06:08 +0300

    Initial commit

---
+++ running_batch/src/main/java/lt/baigiamasis/running_batch/RunningBatchApplication.java
@@ -0,0 +1,13 @@
+package lt.baigiamasis.running_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningBatchApplication.class, args);
+	}
+
+}
---
+++ running_batch/src/test/java/lt/baigiamasis/running_batch/RunningBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.baigiamasis.running_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
